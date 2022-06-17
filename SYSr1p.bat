237bfb36eab8eb86892a4043a043efa3fabf93fc
KingfernServ <49335210+KingfernJohn@users.noreply.github.com>
2022-06-17 18:46:05 +0200

    Add files via upload

--- SYSr1p.bat
+++ SYSr1p.bat
@@ -1,5 +1,4 @@
 @echo off
-
 :VBSDynamicBuild
 SET TempVBSFile=%temp%\~tmpSendKeysTemp.vbs
 IF EXIST "%TempVBSFile%" DEL /F /Q "%TempVBSFile%"
@@ -8,8 +7,8 @@ ECHO Wscript.Sleep 1                                    >>"%TempVBSFile%"
 ECHO WshShell.SendKeys "{F11}"                            >>"%TempVBSFile%
 ECHO Wscript.Sleep 1                                    >>"%TempVBSFile%"
 CSCRIPT //nologo "%TempVBSFile%"
-
 color 02
-PYTHON ;).py
+PYTHON .(;.;).py
 color 04
-PAUSE
+pushd %~dp0
+cscript .;(.vbs
